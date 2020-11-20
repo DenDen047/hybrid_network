eb8d36f4ec4f880ddcafc0ae5e50c96308f8354e
DenDen047 <sh.mn.nat@gmail.com>
2020-11-20 18:44:56 +0900

    fixed bugs

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ IMAGE_NAME="denden047/snn_iir"
 
 docker build -q -t ${IMAGE_NAME} "$CURRENT_PATH"/docker && \
 docker run -it --rm \
-    --gpus 0 \
+    --gpus device=0 \
     -v "$CURRENT_PATH"/src:/workdir \
     -v "$CURRENT_PATH"/dataset:/dataset \
     -v "$CURRENT_PATH"/checkpoint:/checkpoint \
