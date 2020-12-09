36cba9547b582343317b2c7eb86aae47319a7e95
DenDen047 <sh.mn.nat@gmail.com>
2020-12-09 11:17:30 +0900

    update

--- run0.sh
+++ run0.sh
@@ -18,5 +18,10 @@ docker run -it --rm \
             --model networks.cnn_networks_coding.ann4_coding_snn4 \
             --config_file ann_snn_cnn_coding.yaml \
             --train \
+            --logging && \
+        python ann_snn_coding.py \
+            --model networks.cnn_networks_coding.ann6_coding_snn2 \
+            --config_file ann_snn_cnn_coding.yaml \
+            --train \
             --logging \
     "
